bdb9d33b6960e0d95ccb72d6e2dd3b0102be5803
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-27 08:30:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,9 +12,23 @@ class Scraper
     #down another level we have div class card-text-container
     #inside that we want <h4 class="student-name"
     #and <p class student-location
-    doc.css(".student-card") #this should give me an "nodeset" of all those student card items
-    binding.pry
-  end
+    xmlnodeset = doc.css(".student-card") #this should give me an "nodeset" of all those student card items
+    # doc.css(".student-card")[0] #this gives me the first student card
+    #when we execute, we loop thru, so lets pretend we're working on just one item at a time
+    #when u extract an element with css, u can further do css on top of it! like chaining.
+    # doc.css(".student-card")[0].css("a").attribute("href").value
+    # doc.css(".student-card")[0].css("a .card-text-container .student-name").text
+    # doc.css(".student-card")[0].css("a .card-text-container .student-location").text
+    arrayofhashes = []
+    xmlnodeset.each do | student |
+      [] << {
+        :name => student.css("a").attribute("href").value,
+        :location => student.css("a").attribute("href").value,
+        :profile_url => student.css("a").attribute("href").value
+      }
+    end #end iteration
+
+  end #end method
 
   def self.scrape_profile_page(profile_url)
 
