13a769ace0f377dcad6c35fbf90228a9a45f2e64
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-27 08:32:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,8 +22,8 @@ class Scraper
     arrayofhashes = []
     xmlnodeset.each do | student |
       arrayofhashes << {
-        :name => student.css("a").attribute("href").value,
-        :location => student.css("a").attribute("href").value,
+        :name => student.css("a .card-text-container .student-name").text,
+        :location => student.css("a .card-text-container .student-location").text,
         :profile_url => student.css("a").attribute("href").value
       }
     end #end iteration
